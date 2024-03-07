bcb2eb1c275c0a470d230a724ec1fafd85e933b6
kamilnaja <kamilnaja@gmail.com>
2024-03-07 22:45:39 +0100

    reading data to plot

--- learn.py
+++ learn.py
@@ -1,6 +1,22 @@
-# read file using pandas
+import matplotlib.pyplot as plt
 import pandas as pd
 
-# read file using pandas
 data = pd.read_csv('/Users/kamilnaja/projects/ml/homePrices/prices.csv')
-print(data)
+data['Cena'] = data['Cena'].str.replace(
+    ',00 zł', '')
+data['Cena'] = data['Cena'].str.replace(' ', '').astype(int)
+
+
+data['Powierzchnia'] = data['Powierzchnia'].str.replace(
+    ',', '').astype(int)
+
+# pad 0 to right, if length is less than 4 for Powierzchnia
+data['Powierzchnia'] = data['Powierzchnia'].astype(str).str.pad(
+    width=4, side='right', fillchar='0')
+
+# sort by Powierzchnia
+data.sort_values(by='Powierzchnia', inplace=True)
+print(data.head())
+plt.scatter(data['Powierzchnia'], data['Cena'], color='red', marker='+')
+plt.xticks(rotation=90)
+plt.show()
