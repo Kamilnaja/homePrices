bcdb5c9c6518e7d8166dff8605dd723191cda66b
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2024-03-09 09:33:04 +0100

    Fix data formatting in learn.py and update linear regression model

--- learn.py
+++ learn.py
@@ -5,7 +5,7 @@ import sklearn.linear_model
 
 data = pd.read_csv("prices.csv")
 data["Cena"] = data["Cena"].str.replace(",00 zł", "")
-data["Cena"] = data["Cena"].str.replace(" ", "").astype(int)
+data["Cena"] = data["Cena"].str.replace(" ", "").astype(int)
 
 
 data["Powierzchnia"] = (
@@ -15,14 +15,21 @@ data["Powierzchnia"] = (
 )
 data["Powierzchnia"] = data["Powierzchnia"].astype(int)
 
-print(data.head())
+powierzchnia_median = data["Powierzchnia"].median()
+cena_median = data["Cena"].median()
 
-data.plot(kind="scatter", y="Powierzchnia", x="Cena")
-plt.show()
+print(powierzchnia_median, cena_median)
+print(data.head())
 
 X = data[["Powierzchnia"]]
 y = data["Cena"]
 
 model = sklearn.linear_model.LinearRegression()
 model.fit(X, y)
-X_new = [[3000]]
+X_new = [[2678.5]]
+print(model.predict(X_new))
+
+# plot linear regression
+plt.scatter(X, y, color="black")
+plt.plot(X, model.predict(X), color="blue", linewidth=3)
+plt.show()
